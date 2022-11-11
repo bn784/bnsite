d531e447445e262cf01b053aba56105dc8cc8095
bn <bn7854420@gmail.com>
2022-11-11 10:50:05 +0200

    10.49

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,16 +22,15 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container-fluid">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <button id="Toggle_Sidebar" class=" btn btn-light nav-link"> Toggle Sidebar</button>
+                         
+            
+                <a class="navbar-brand nav-link" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+               
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
