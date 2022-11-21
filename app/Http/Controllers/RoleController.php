c637af5f840e2c466b5021bc1d5d0d9c06c47c5a
bn <bn7854420@gmail.com>
2022-11-21 16:38:20 +0200

    role checkbox

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -98,7 +98,7 @@ class RoleController extends Controller
     public function update(Request $request, $id)
     {
         
-        //dd($users, $roles);
+        
         $role = Role::findOrFail($id);
         if ($role->title == 'administrator' ) {
             return redirect()->route('roles.index')->with('warning', 'Cannot update administrator!');
@@ -107,7 +107,64 @@ class RoleController extends Controller
             return redirect()->route('roles.index')->with('warning', 'Cannot update user!');
         }
 
-        $role->update($request->all());
+        
+        if ($request->management_access == '1' ) {
+            $role->management_access = '1';
+        } else {
+            $role->management_access = '0'; 
+        }
+        if ($request->user_access == '1' ) {
+            $role->user_access = '1';
+        } else {
+            $role->user_access = '0'; 
+        }
+        if ($request->user_create == '1' ) {
+            $role->user_create = '1';
+        } else {
+            $role->user_create = '0'; 
+        }
+        if ($request->user_edit == '1' ) {
+            $role->user_edit = '1';
+        } else {
+            $role->user_edit = '0'; 
+        }
+        if ($request->user_view == '1' ) {
+            $role->user_view = '1';
+        } else {
+            $role->user_view = '0'; 
+        }
+        if ($request->user_delete == '1' ) {
+            $role->user_delete = '1';
+        } else {
+            $role->user_delete = '0'; 
+        }
+        if ($request->role_access == '1' ) {
+            $role->role_access = '1';
+        } else {
+            $role->role_access = '0'; 
+        }
+        if ($request->role_create == '1' ) {
+            $role->role_create = '1';
+        } else {
+            $role->role_create = '0'; 
+        }
+        if ($request->role_edit == '1' ) {
+            $role->role_edit = '1';
+        } else {
+            $role->role_edit = '0'; 
+        }
+        if ($request->role_view == '1' ) {
+            $role->role_view = '1';
+        } else {
+            $role->role_view = '0'; 
+        }
+        if ($request->role_delete == '1' ) {
+            $role->role_delete = '1';
+        } else {
+            $role->role_delete = '0'; 
+        }
+        
+        $role->update();
 
         return redirect()->route('roles.index')->with('success', $role->title.' update successfully!');
     }
