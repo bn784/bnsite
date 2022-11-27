9d4fc10d094f0ed0209f67bee5d44b3320da49c7
bn <bn7854420@gmail.com>
2022-11-27 10:25:30 +0200

    jq welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,31 +8,59 @@
 {{ App::setLocale('UA') }}
 @endif
 @endauth
+
+<style type="text/css">
+   
+</style>
+
 @section('content')
+
 <div class="container-fluid">
     <div id="row1" class="row row1 rowbnsite" style="background-color:blue;">
-     <div class="col row1col1" style="background-color:blue;">
+     <div class="col row1col1">
      row1col1
      </div>
-     <div class="col row1col2"  style="background-color:blue;">
+     <div class="col row1col2">
       row1col2
      </div>
     </div>
-    <div id="row2" class="row row2 rowbnsite"  style="background-color:red;">
+    <div id="row2" class="row row2 rowbnsite bnsiteChange">
+    
 	row2
     </div>
-    <div id="row3" class="row row3 rowbnsite"  style="background-color:grey;">
+    <div id="row3" class="row row3 rowbnsite bnsiteChange">
 	row3
     </div>
-    <div id="row4" class="row row4 rowbnsite"  style="background-color:green;">
+    <div id="row4" class="row row4 rowbnsite bnsiteChange">
 	row4
     </div>
-    <div id="row5" class="row row5 rowbnsite"  style="background-color:brown;">
+    <div id="row5" class="row row5 rowbnsite bnsiteChange">
 	row5
     </div>
-    <div id="row6" class="row row6 rowbnsite"  style="background-color:pink;">
+    <div id="row6" class="row row6 rowbnsite bnsiteChange">
 	row6
     </div>
 </div>
+
 @endsection
 
+<!-- javascripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+/*
+$(document).ready(function(){
+  $(".bnsiteChange").hover(function(){
+  //$(".bnsiteChange").mouseover(function(){
+  //$(this).css("background-color", "yellow");
+  $(this).addClass("hover"); //добавляем класс текущей
+  });
+  $(".bnsiteChange").mouseleave(function(){  
+    $(this).removeClass("hover"); //удаляем класс текущей
+    //$(this).css("background-color", "brown");
+  });
+  $(".bnsiteChange").click(function(){
+    $(this).addClass("active"); //добавляем класс текущей
+  });
+});*/
+</script>
