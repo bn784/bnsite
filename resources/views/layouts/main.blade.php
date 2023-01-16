33ab584e6d746deacfbf1ed58ccf5c12bea76a6d
bogdan <39177903+bn784@users.noreply.github.com>
2023-01-16 12:03:41 +0200

    12.00 16.01.2023

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,24 +10,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'bnsite') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">  
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!--<link rel="dns-prefetch" href="//fonts.gstatic.com">-->
+    <!--<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">-->
 
      <!-- javascripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <!-- Styles -->
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 
+    <!-- Styles -->
 <style type="text/css">
 .main {
     left: 0;
@@ -80,7 +80,7 @@ div.rowbnsite {
 }
 </style>
    
-       
+<title>{{ config('app.name', 'bnsite') }}</title>
 </head>
 <body>
     <div id="app">
